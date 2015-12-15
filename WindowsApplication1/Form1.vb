f219a456fded77e250cf0204d0d685e9244eee81
Earlsan <earlsan.villegas@spectralasia.net>
2015-12-15 13:56:12 +0800

    Revert "Merger First Changes"

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,11 +1,4 @@
-﻿Import System.Windows.Forms
-Public Class Form1
-
-	Public Sub PrintSomething()
-
-		MessageBox.Show("HEllo, World")
-
-	End Sub
+﻿Public Class Form1
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
         MsgBox("button 1")
