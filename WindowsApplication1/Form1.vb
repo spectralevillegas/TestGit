8a7d435c2c4e68e2280fb631c3da7737f0c5108b
spectralevillegas <earlsan.villegas@spectralasia.net>
2015-12-15 11:48:47 +0800

    Message Btn 2

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
         MsgBox("button 1")
     End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        MsgBox("button 2")
+    End Sub
 End Class
